1efdf0e28d734238c6945b257571417fa341efed
Devin Green <dev.green2@gmail.com>
2025-04-19 00:50:48 -0700

    Update test to reflect application infrastructure

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,21 @@
-from cliapp import ApplicationConfig
-import json
+from cliapp import Application
+from cliapp import Command
+import asyncio
 
-def main():
-    config: ApplicationConfig = ApplicationConfig.fromPath("tests/config.json")
-    print()
+async def test(*args, n=False, t=False):
+    print(*args)
+    await asyncio.sleep(5)
 
 if __name__ == "__main__":
-    main()
+    app = Application(config="tests/config.json")
+    
+    command = Command("test", test)
+    command.addFlag("n", help="Should show number result")
+    command.addFlag("t")
+    
+    app.add(command)
+    
+    app.run()
+    
+    
+# sub-zero, larry3d, ansi_shadow, ansi_regular, big-money-ne
